bd0cb0863488e51c7510f3047b4558844538ce1c
ハマヤ永井 <hamayanagai@MacBook-Pro-2.local>
2022-03-26 01:30:41 +0900

    Create Header

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import Tailwindcss from './Tailwindcss.svelte';
+	import Header from './components/Header.svelte';
+
+	export let name: string;
+</script>
+
+<Tailwindcss />
+<Header />
+<main class="pt-16">
+	<div class="container mx-auto px-6 my-4">
+		<h1>Hello {name}!</h1>
+		<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	</div>	
+</main>
+
