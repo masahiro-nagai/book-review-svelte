94283efb80cbbb44a488411b11b0134f24e64c4d
ハマヤ永井 <hamayanagai@MacBook-Pro-2.local>
2022-03-28 23:54:43 +0900

    ストアの作成

--- src/pages/SearchBook.svelte
+++ src/pages/SearchBook.svelte
@@ -1,74 +1,67 @@
 <script lang="ts">
-    import SearchBar from '../components/SearchBar.svelte'
-    import type { BookItem, Result } from '../repositories/book';
-    import RepositoryFactory, { BOOK } from '../repositories/RepositoryFactory'
-    import Spinner from '../components/Spinner.svelte'
-    import BookCard from '../components/BookCard.svelte';
-    import InfiniteScroll from "svelte-infinite-scroll"
-    const BookRepository = RepositoryFactory[BOOK]
-  
-    let q = ''
-    let empty = false
-    let books: BookItem[] = []
-    let promise: Promise<void>
-    let startIndex = 0
-  
-    const handleSubmit = () => {
-      if (!q.trim()) return
-      promise = getBooks()
-    }
+ import SearchBar from '../components/SearchBar.svelte'
+ import Spinner from '../components/Spinner.svelte'
+ import BookCard from '../components/BookCard.svelte'
+ import RepositoryFactory, { BOOK } from '../repositories/RepositoryFactory'
+ import InfiniteScroll from "svelte-infinite-scroll"
+ import { books } from '../store/book'
+ const BookRepository = RepositoryFactory[BOOK]
 
-    let totalItems = 0
+ let q = ''
+ let empty = false
+ let promise: Promise<void>
+ let totalItems = 0
+ let startIndex = 0
 
-    $: hasMore = totalItems > books.length
-  
-    const getBooks = async () => {
-      books = []
-      empty = false
-      startIndex = 0
-      const result = await BookRepository.get({ q })
-      empty = result.totalItems === 0
-      totalItems = result.totalItems
-      books = result.items
-    }
+ $: hasMore = totalItems > $books.length
 
-    const handleLoadMore = () =>{
-        startIndex += 10
-        promise = getNextPage()
-    }
+ const handleSubmit = () => {
+   if (!q.trim()) return
+   promise = getBooks()
+ }
 
-    const getNextPage = async() =>{
-        const result = await BookRepository.get({q,startIndex})
-        console.log(result)
-        console.log(result.items)
-        console.log(startIndex)
-        // idでフィルタリングする
-        const bookIds = books.map(book => book.id)
-        const filteredItems = result.items.filter(item=>{
-            return !bookIds.includes(item.id)
-            
-        })
-        books =[...books,...filteredItems]
-    }
-  </script>
-  
-  <form on:submit|preventDefault={handleSubmit}>
+ const getBooks = async () => {
+    $books = []
+   empty = false
+   startIndex = 0
+   const result = await BookRepository.get({ q, startIndex })
+   empty = result.totalItems === 0
+   totalItems = result.totalItems
+    $books = result.items
+ }
+
+ const handleLoadMore = () => {
+   startIndex += 10
+   promise = getNextPage()
+ }
+
+ const getNextPage = async () => {
+   const result = await BookRepository.get({ q, startIndex })
+
+   // 取得データが既に存在するものを含む可能性があるので、フィルタリングしてます。
+    const bookIds = $books.map(book => book.id)
+   const filteredItems = result.items.filter(item => {
+     return !bookIds.includes(item.id)
+   })
+    $books = [...$books, ...filteredItems]
+ }
+</script> 
+<form on:submit|preventDefault={handleSubmit}>
     <SearchBar bind:value={q} />
   </form>
-  
   <div class="text-center mt-4">
     {#if empty}
       <div>検索結果が見つかりませんでした。</div>
     {:else}
     <div class="grid grid-cols-1 gap-2 lg:grid-cols-2">
-      {#each books as book (book.id)}
-      <BookCard {book} />
+      {#each $books as book (book.id)}
+        <BookCard {book} />
       {/each}
     </div>
     <InfiniteScroll window threshold={100} on:loadMore={handleLoadMore} {hasMore} />
     {/if}
     {#await promise}
-    <div class="flex justify-center">
+      <div class="flex justify-center">
         <Spinner />
       </div>
       {:catch e}
@@ -77,3 +70,4 @@
         </span>
     {/await}
   </div>
+  
